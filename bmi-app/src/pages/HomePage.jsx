be6fb2d146538c4e213098414d393334df960cca
blazejpak <blazej.pak@gmail.com>
2023-05-17 00:01:21 +0200

    started section hero

---
+++ bmi-app/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionHero from "../components/SectionHero";
+
+const HomePage = () => {
+  return (
+    <main>
+      <SectionHero />
+    </main>
+  );
+};
+
+export default HomePage;
